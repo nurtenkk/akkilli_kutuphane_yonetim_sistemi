c3ff643ed44e0e62ead061d96de37b6a51cb745d
NurtenKaval <kavalnurten@gmail.com>
2025-12-25 14:51:42 +0300

    Add files via upload

---
+++ src/test/java/com/kutuphane/project/LibrarySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kutuphane.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
